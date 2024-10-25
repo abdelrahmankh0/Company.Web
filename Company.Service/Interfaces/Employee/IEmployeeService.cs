4efcac17cc8b6f18a2ba6840ca01a9d96ad92a83
abdelrahmankh0 <abosh5586@gmail.com>
2024-10-25 22:19:31 +0300

    from EmployeeController(index-withsearch) To AccountController(SignOut)

---
+++ Company.Service/Interfaces/Employee/IEmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using Company.Data.Models;
+using Company.Service.Interfaces.Employee.Dto;
+
+namespace Company.Service.Interfaces
+{
+
+	public interface IEmployeeService
+	{
+		EmployeeDto GetById(int? id);
+
+		IEnumerable<EmployeeDto> GetAll();
+
+		void Add(EmployeeDto entity);
+
+		void Update(EmployeeDto entity);
+
+		void Delete(EmployeeDto entity);
+		IEnumerable<EmployeeDto> GetEmployeeByName(string name);
+	}
+}
+
---
+++ Company.Web/Models/SignUpViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.Models
+{
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage ="First Name Is Required ")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name Is Required ")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Invalid Format For Email ")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password Is Required ")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$",
+        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, one special character, and be at least 8 characters long.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm Password Is Required ")]
+        [Compare(nameof(Password), ErrorMessage = "Confirm Password does not match password ")]
+
+        public string ConfirmPassword {  get; set; }
+        [Required(ErrorMessage ="Required to Agree")]
+        public bool IsAgree { get; set; }
+
+    }
+}
